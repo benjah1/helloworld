8a5d7b12a62728b05b585d736250cdb7bc728e65
benjah1 <benjaminhuang1@gmail.com>
2014-11-17 13:26:17 -0500

    apply jshint to test case and fix error

--- test/js/builder.js
+++ test/js/builder.js
@@ -6,15 +6,15 @@ describe('Test Builder', function () {
 	it('create product', function() {
 		var builder = new Builder();
 		builder.set({
-			a: "ba",
-			b: "bb",
-			c: "bc"
+			a: 'ba',
+			b: 'bb',
+			c: 'bc'
 		});
 
 		var product = builder.build();
 
-		expect(product.a).to.be.equal("ba");
-		expect(product.b).to.be.equal("bb");
+		expect(product.a).to.be.equal('ba');
+		expect(product.b).to.be.equal('bb');
 		expect(typeof product.c).to.be.equal('undefined');
 	});
 });
