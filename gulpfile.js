d73e1dc58f07fd0c55485f5d56e5a75d94125589
benjah1 <benjaminhuang1@gmail.com>
2014-11-17 11:59:25 -0500

    got something on coverage report. no perfect yet

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 
 // load plugins
 var $ = require('gulp-load-plugins')();
+var    cover = require('gulp-coverage');
 
 gulp.task('jslint', function () {
 	return gulp.src('js/**/*.js', {base: '.'})
@@ -16,24 +17,84 @@ gulp.task('jslint', function () {
 });
 
 gulp.task('jsBuildTest', ['jslint', 'jsClean'], function() {
-	return gulp.src([
-		'test/js/**/*.js'
-	], {base: '.'})
-		.pipe($.browserify({
-			debug: true
-		}))
- 		.pipe(gulp.dest('./.tmp'))
+	
 
 });
 
-gulp.task('jsTest', ['jsBuildTest'], function() {
-	return gulp.src([
-		'.tmp/**/*.js'
-	], {base: '.'})
-		.pipe($.karma({
-			configFile: 'karma.conf.js',
-			action: 'run'
-		}));
+gulp.task('jsTest', ['jsBuildTest'], function(cb) {
+
+  var istanbul = require("gulp-istanbul");
+	var browserify = require('browserify');
+	var transform = require('vinyl-transform');
+
+	gulp.src(['test/js/*.js'])
+		.pipe(transform(function(filename) {
+			return browserify(filename).bundle();
+		}))
+		.pipe(gulp.dest('./.tmp'))
+		.on('finish', function() {
+			gulp.src(['.tmp/**/*.js'])
+		    // .pipe(istanbul())
+				.pipe($.karma({
+					configFile: 'karma.conf.js',
+					action: 'run'
+				}))
+				// .pipe(istanbul.writeReports())
+		    .on('end', cb);
+
+		})
+
+
+  //   gulp.src('js/**/*.js')
+  //   .pipe(istanbul())
+  //   .on('finish', function () {
+		// 	gulp.src(['test/js/*.js'])
+  //  		//  .pipe(transform(function(filename) {
+		//  //    return browserify(filename).bundle();
+		//  //  }))
+		// 	// .pipe(gulp.dest('./.tmp'))
+		// 	.pipe($.karma({
+		// 		configFile: 'karma.conf.js',
+		// 		action: 'run'
+		// 	}))
+    
+		// 	.pipe(istanbul.writeReports())
+  //     .on('end', cb);
+		// });
+
+// var karma = require('karma').server;
+// .pipe(karma.start({
+// 	    configFile: __dirname + '/karma.conf.js',
+// 	    singleRun: true
+// 	  }, cb))
+
+	// gulp.src([
+	// 	'js/**/*.js',
+	// 	'test/js/**/*.js'
+	// ], {base: '.', read: false})
+	// 	.pipe($.browserify({
+	// 		debug: false
+	// 	}))
+	// 	// .pipe(gulp.dest('./.tmp'))
+	// //   .pipe(cover.instrument({
+	// //       pattern: ['js/**/*.js'],
+	// //       debugDirectory: 'debug'
+	// //   }))
+	// .pipe($.karma({
+	// 	configFile: 'karma.conf.js',
+	// 	action: 'run'
+	// }))
+ //    .pipe(cover.report({
+ //        outFile: 'coverage.html'
+ //    }));
+
+	// var karma = require('karma').server;
+
+	// karma.start({
+	// 	configFile: __dirname + '/karma.conf.js',
+	// 	singleRun: true
+	// }, done);
+ 
 });
 
 gulp.task('jsClean', function () {
--- karma.conf.js
+++ karma.conf.js
@@ -10,12 +10,13 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['browserify','mocha','chai'],
+    frameworks: ['mocha','chai'],
 
 
     // list of files / patterns to load in the browser
     files: [
-        '.tmp/**/*.js'
+        // 'js/**/*.js',
+        // 'test/js/**/*.js'
     ],
 
 
@@ -27,15 +28,16 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'js/*.js': ['coverage'],
-//        'test/js/*.js': ['browserify']
+        // 'js/**/*.js': ['coverage', 'browserify']
+        // 'test/js/*.js': ['browserify']
+        '.tmp/**/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['progress','coverage'],
 
 
     // web server port
@@ -62,6 +64,16 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true,
+
+    coverageReporter: {
+        reporters: [
+            { type: 'html', dir: 'coverage' },
+            { type: 'lcovonly', dir: 'coverage' },
+            { type: 'json', dir: 'coverage' },
+            { type: 'cobertura', dir: 'coverage' }
+        ]
+    }
+
   });
 };
