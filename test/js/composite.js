23377b32aea7d23a5c0ec408f7fe93c92df34c6c
benjah1 <benjaminhuang1@gmail.com>
2014-11-17 23:33:10 -0500

    update test case for code coverage

--- test/js/composite.js
+++ test/js/composite.js
@@ -16,13 +16,13 @@ describe('Test Composite', function() {
 		};
 	}, Composite);
 		
-	it('create conponent', function() {
-		var item0 = new Item('item0'),
-			item1 = new Item('item1'),
-			item2 = new Item('item2'),
-			item3 = new Item('item3'),
-			item4 = new Item('item4');
+	var item0 = new Item('item0'),
+		item1 = new Item('item1'),
+		item2 = new Item('item2'),
+		item3 = new Item('item3'),
+		item4 = new Item('item4');
 
+	it('test add', function() {
 		item0.add(item1);
 		item0.add(item2);
 		item1.add(item3);
@@ -34,17 +34,17 @@ describe('Test Composite', function() {
 		assert.strictEqual(item3.size(),1);
 		assert.strictEqual(item4.size(),0);
 
-		it('test loop', function() {
-			assert.strictEqual(item0.loop(), 'item0 item1 item3 item4 item2');
-		});
-
-		it('test remove', function() {
-			item3.remove(item4);
-
-			assert.strictEqual(item3.size(),0);
-			assert.strictEqual(item0.loop(), 'item0 item1 item3 item2');
-
-		});		
 	});
 
+	it('test loop', function() {
+		assert.strictEqual(item0.loop(), 'item0 item1 item3 item4 item2');
+	});
+
+	it('test remove', function() {
+		item3.remove(item4);
+
+		assert.strictEqual(item3.size(),0);
+		assert.strictEqual(item0.loop(), 'item0 item1 item3 item2');
+	});
+	
 });
